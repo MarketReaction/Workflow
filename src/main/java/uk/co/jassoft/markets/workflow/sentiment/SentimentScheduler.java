a66b4cef5e9e56cebd54cf298db35eb99fd95b54
Jon Shaw <jon.shaw@jassoft.co.uk>
2016-12-09 19:56:57 +0000

    added SENTIMENT_API_REST_URL when launchtin sentiment

--- src/main/java/uk/co/jassoft/markets/workflow/sentiment/SentimentScheduler.java
+++ src/main/java/uk/co/jassoft/markets/workflow/sentiment/SentimentScheduler.java
@@ -1,16 +1,19 @@
 package uk.co.jassoft.markets.workflow.sentiment;
 
+import org.springframework.beans.factory.annotation.Value;
 import uk.co.jassoft.markets.workflow.AbstractDockerScheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.TextMessage;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by jonshaw on 02/09/2016.
@@ -20,7 +23,17 @@ public class SentimentScheduler extends AbstractDockerScheduler {
 
     private static final Logger LOG = LoggerFactory.getLogger(SentimentScheduler.class);
 
-    @JmsListener(destination = "MatchFound", concurrency = "5")
+    @Value("${SENTIMENT_API_REST_URL}")
+    private String sentimentUrlRestUrl;
+
+    private final List<String> envs = new ArrayList<>();
+
+    @PostConstruct
+    private void setup() {
+        envs.add("SENTIMENT_API_REST_URL=" + sentimentUrlRestUrl);
+    }
+
+    @JmsListener(destination = "MatchFound")
     public void onMessage(final Message message) throws JMSException {
         final Date start = new Date();
         if (message instanceof TextMessage) {
@@ -28,7 +41,7 @@ public class SentimentScheduler extends AbstractDockerScheduler {
 
             LOG.info("Triggering Senitment as MatchFound");
 
-            scheduleContainer(SENTIMENT_IMAGE, new ArrayList<String>(), 100l, textMessage.getText());
+            scheduleContainer(SENTIMENT_IMAGE, envs, 100l, textMessage.getText());
         }
     }
 }
