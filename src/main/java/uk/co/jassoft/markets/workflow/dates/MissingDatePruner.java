00e41225e4047bda4204e1983abf1020d87ec3e3
Jon Shaw <jon.shaw@jassoft.co.uk>
2016-12-22 10:05:02 +0000

    dont run missing date pruner

--- src/main/java/uk/co/jassoft/markets/workflow/dates/MissingDatePruner.java
+++ src/main/java/uk/co/jassoft/markets/workflow/dates/MissingDatePruner.java
@@ -1,19 +1,19 @@
 package uk.co.jassoft.markets.workflow.dates;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.scheduling.annotation.Scheduled;
 import uk.co.jassoft.markets.datamodel.story.date.MissingDateFormat;
 import uk.co.jassoft.markets.repository.MissingDateFormatRepository;
 import uk.co.jassoft.markets.utils.article.ContentGrabber;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 /**
  * Created by jonshaw on 22/01/15.
  */
-@Component
+//@Component
 @Import({ContentGrabber.class})
 public class MissingDatePruner {
 
@@ -27,7 +27,7 @@ public class MissingDatePruner {
     public void prune()
     {
         // TODO this needs to be paged
-        for(MissingDateFormat missingDateFormat : missingDateFormatRepository.findAll()) {
+        for(MissingDateFormat missingDateFormat : missingDateFormatRepository.findAll(new PageRequest(0, 100))) {
             Date publishedDate = contentGrabber.getPublishedDate(missingDateFormat.getMetatag());
 
             if(publishedDate != null) {
